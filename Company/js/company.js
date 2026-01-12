6ea18b2df774f3410965a830661b967d678da011
Samruddhi <samruddhispatil27@gmail.com>
2026-01-13 00:26:45 +0530

    Final commit

---
+++ Company/js/company.js
@@ -0,0 +1,91 @@
+// Start from control coding
+var signupBtn = document.querySelector(".signup-btn");
+var loginBtn = document.querySelector(".login-btn");
+var loginBox = document.querySelector(".login-box");
+var signupBox = document.querySelector(".signup-box");
+
+signupBtn.onclick = function(){
+    signupBox.classList.add("active");
+    loginBox.classList.remove("active")
+    loginBtn.classList.remove("d-none");
+    signupBtn.classList.add("d-none")
+}
+loginBtn.onclick = function () {
+  signupBox.classList.remove("active");
+  loginBox.classList.add("active");
+  loginBtn.classList.add("d-none");
+  signupBtn.classList.remove("d-none");
+};
+
+//start register coding
+var registerForm = document.querySelector(".signup-form");
+var allInput = registerForm.querySelectorAll("INPUT");
+var textArea = registerForm.querySelector("textarea");
+
+registerForm.onsubmit = function(e){
+    e.preventDefault();
+    registrationData();
+
+}
+
+const registrationData = () =>{
+    if(localStorage.getItem(allInput[0].value+"_brand") == null){
+        const userData = {
+          brandCode: allInput[0].value,
+          brandName: allInput[1].value,
+          website: allInput[2].value,
+          contact: allInput[3].value,
+          address: textArea.value,
+          username: allInput[4].value,
+          password: allInput[5].value,
+        };
+        let userString = JSON.stringify(userData);
+        localStorage.setItem(allInput[0].value+"_brand", userString);
+        registerForm.reset('');
+        swal("Registration Done!", "Next step do Sign In!", "success");
+    }else{
+        swal(
+          "Please Change Brand Code!",
+          "This brand code already taken !",
+          "warning"
+        );
+    }
+}
+
+//Start signin coding
+var signinBtn = document.querySelector(".signin-btn");
+var brandCode = document.querySelector("#brand-code");
+var username = document.querySelector("#username");
+var password = document.querySelector("#password");
+
+signinBtn.onclick = function(e){
+    e.preventDefault();
+    if(brandCode.value && username.value && password.value != ""){
+        if(localStorage.getItem(brandCode.value+"_brand") != null){
+            var allData = JSON.parse(localStorage.getItem(brandCode.value+"_brand"));
+            console.log(allData);
+            if(allData.username == username.value){
+                if(allData.password == password.value){
+                    signinBtn.innerHTML = "Please wait...";
+                    signinBtn.disabled = true;
+                    setTimeout(function(){
+                        window.location = "../Dashboard/dashboard.html";
+                        sessionStorage.setItem("brandCode",brandCode.value)
+                    },3000)//3000 = 3sec
+                }else{
+                    swal("Wrong Password!", "Please check the password", "warning");
+                }
+            }else{
+                swal("Wrong Username!", "Please check the username", "warning");
+            }
+        }else{
+            swal("Wrong Brand Code !", "Please signup first ! ", "warning");
+        }
+    }else{
+        swal(
+          "Empty field !",
+          "Please fill all the field",
+          "warning"
+        );
+    }
+}
